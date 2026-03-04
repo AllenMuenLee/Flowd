058d345fc950c07953dee2bc74b50bad6d3fb4bd
Allen Lee <limuen.allen@gmail.com>
2026-03-03 23:07:25 -0500

    Revert "Add files via upload"
    This reverts commit 4be9fbe7d77d3c0fd033eb7ca02577cbe1582ae8.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import sys
+
 from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout
+
 from app.pages.projectBuilder import ProjectBuilderWidget
 from app.pages.canva import CanvaWidget
 
@@ -7,39 +9,29 @@ from app.pages.canva import CanvaWidget
 def main():
     app = QApplication(sys.argv)
     window = QWidget()
-    window.setWindowTitle("Vibe Coding App")  # Better title
+    window.setWindowTitle("Project Builder")
     window.resize(900, 600)
-    
+
     layout = QVBoxLayout(window)
     layout.setContentsMargins(0, 0, 0, 0)
-    
-    # Create canvas (will be empty initially)
+
     canvas = CanvaWidget()
     canvas.hide()
-    
+
     def on_project_created(success):
         if success:
-            # Hide builder
             builder.hide()
-            
-            # Recreate canvas with new project data
-            layout.removeWidget(canvas)
-            canvas.deleteLater()
-            
-            new_canvas = CanvaWidget()
-            layout.addWidget(new_canvas)
-            new_canvas.show()
-            new_canvas.raise_()
+            canvas.show()
+            canvas.raise_()
         else:
-            # Show error, keep builder visible
             canvas.hide()
             builder.show()
-    
+
     builder = ProjectBuilderWidget(on_project_created=on_project_created)
-    
+
     layout.addWidget(builder)
     layout.addWidget(canvas)
-    
+
     window.show()
     sys.exit(app.exec())
 
